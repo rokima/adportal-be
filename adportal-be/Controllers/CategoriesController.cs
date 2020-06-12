be1866330582365ef2068e5903d27188705e5608
Ronald Kimask <rokima@ttu.ee>
2020-06-12 21:13:45 +0300

    add categories controller

---
+++ adportal-be/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using adportal_be.Data;
+using adportal_be.Models;
+
+namespace adportal_be.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+
+        //Get all categories
+        AdPortalDbContext adPortalDbContext = new AdPortalDbContext();
+
+        public IHttpActionResult Get()
+        {
+            var categories = adPortalDbContext.Categories;
+            return Ok(categories);
+        }
+
+    }
+}
